45315ef2bda1830eed585ef224c3d4f9d34928db
Leoni <leoni.teixeira@hotmail.com>
2020-07-04 15:06:55 -0300

    modificando extensao do app

--- src/data/todo.js
+++
@@ -1,5 +0,0 @@
-export default [
-    {id: 1, atividade: 'Fazer uma corrida', status: 1},
-    {id: 2, atividade: 'Fazer uma casa', status: 1},
-    {id: 3, atividade: 'Fazer uma torta', status: 1},
-]
