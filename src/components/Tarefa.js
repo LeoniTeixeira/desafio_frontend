2c905a07ec7e5005ef45422890c1e2759fba8760
Leoni <leoni.teixeira@hotmail.com>
2020-07-04 15:45:10 -0300

    criacao da lista de tarefas

--- src/components/Tarefa.js
+++ src/components/Tarefa.js
@@ -3,9 +3,17 @@ import React, { Component } from 'react'
 export default class Tarefa extends Component {
     render() {
         return (
-            <div>
-                Tarefa
-            </div>
+            <li className='list-group-item text-capitalize d-flex 
+              justify-content-between my-4 flex-wrap'>
+                <input type="checkbox" className='my-auto'/>
+                <h6 className='my-auto'>descricao</h6>
+                <div className='my-auto'>
+                    <button type='submit' className='btn btn-success 
+                      my-auto text-capitalize'>editar</button>
+                    <button type='submit' className='btn btn-danger 
+                      ml-1 my-auto text-capitalize'>Apagar</button>
+                </div>
+            </li>
         )
     }
 }
