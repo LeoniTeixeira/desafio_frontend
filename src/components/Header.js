7f64f40e633f48833e419425ababa290f1394a58
Leoni <leoni.teixeira@hotmail.com>
2020-07-04 16:47:40 -0300

    criado o botão para mudança de tema

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,8 +3,11 @@ import React, { Component } from 'react'
 export default class Header extends Component {
     render() {
         return (
-                <header className='container-fluid py-3 bg-primary shadow-lg'>
-                    <h4 className='text-light'>To Do List</h4>
+                <header className='container-fluid py-3 bg-primary shadow-lg '>
+                    <row className='d-flex justify-content-between'>
+                        <h4 className='text-light my-auto'>To Do List</h4>
+                        <h4 className='text-light my-auto btn'>Tema</h4>
+                    </row>
                 </header>
         )
     }
