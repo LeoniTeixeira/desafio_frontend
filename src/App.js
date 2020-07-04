1366da2b36c0c4d00a960cf61463b0051dd915f8
Leoni <leoni.teixeira@hotmail.com>
2020-07-04 15:15:16 -0300

    correcao de caminho do header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import React from 'react'
-import Header from './components/layout/Header'
-import Main from './components/layout/Main'
+import React, { Component } from 'react'
+import Header from './components/Header'
 
-export default () =>
-    <div>
-    <Header></Header>
-    <Main></Main>
-    </div>
+export default class App extends Component {
+    render() {
+        return (
+            <div>
+                <Header/>
+            </div>
+        )
+    }
+}
