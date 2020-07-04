361440fb28cd70d5dd248f43260fffb7dd1f4d40
Leoni <leoni.teixeira@hotmail.com>
2020-07-04 15:31:30 -0300

    criação do input text

--- src/components/NovaTarefa.js
+++ src/components/NovaTarefa.js
@@ -3,8 +3,13 @@ import React, { Component } from 'react'
 export default class NovaTarefa extends Component {
     render() {
         return (
-            <div>
-                NovaTarefa
+            <div className='card card-body  my-3'>
+                <form>
+                    <input type='text' className='form-control text-capitalize' 
+                        placeholder='adicione uma tarefa'/>
+                    <button type='submit' className='btn btn-block btn-primary 
+                        mt-3 text-capitalize'>adicionar tarefa</button>
+                </form>
             </div>
         )
     }
